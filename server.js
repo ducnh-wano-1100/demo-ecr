018875ac73c0b414c657b4fb960143ba2ea62e62
nguyendev2022 <nguyentuan@team-lab.com>
2023-03-02 01:31:14 -0800

    Fix cicd

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
+  asdasdasd
   res.send(
     `<body style='background-color:#283E5B'><h1 style='color: orange;text-align:center'>Hello Tu Ngu ${os.hostname()}</h1></body>`
   );
